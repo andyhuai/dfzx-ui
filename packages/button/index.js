92fb61def662f6e297f1866d98c8ef349d7f0fbc
Huai <xhhuang@fiberhome.com>
2020-10-10 16:06:20 +0800

    init

--- packages/button/index.js
+++
@@ -1,6 +0,0 @@
-import VsButton from "./src/button.vue";
-// 为组件提供 install 安装方法，供按需引入
-VsButton.install = function(Vue) {
-  Vue.component(VsButton.name, VsButton);
-};
-export default VsButton;
--- types/component.d.ts
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-
-export declare class VaseUIComponent extends Vue {
-    static install(vue: typeof Vue): void
-}
-
-export type VaseUIComponentSize = 'large' | 'medium' | 'small' | 'mini'
-
-export type VaseUIHorizontalAlignment = 'left' | 'center' | 'right'
